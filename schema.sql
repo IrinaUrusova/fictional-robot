685d8b66b54066cddfca838352c20b09508522b6
IrinaUrusova <146344550+IrinaUrusova@users.noreply.github.com>
2026-03-06 23:44:25 +0300

    add users table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ id uuid primary key,
 name text not null,
 created_at timestamptz not null default now()
 );
+create table if not exists users (
+id uuid primary key,
+company_id uuid not null references companies(id),
+name text not null,
+email text not null unique,
+role text not null,
+created_at timestamptz not null default now()
+);
